1b471c2505355ac3872b657f4515007394886ea7
Omar <hoonomar@gmail.com>
2024-05-19 12:43:58 -0400

    terraforn update

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "devops-recipe-app-api-state"
+    bucket         = "bromars-tf-state"
     key            = "tf-state-setup"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "devops-recipe-app-api-tf-lock"
+    dynamodb_table = "bromars-tf-lock"
   }
 }
 
